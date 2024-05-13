5900a2cefdbc310120efcebe39c38697a20428da
Vinicius <vinicius.costa@orram.com.br>
2024-05-13 10:46:33 -0300

    IPersonService Interface Implementation

---
+++ ProjectWithASPNET8/Services/IPersonService.cs
@@ -0,0 +1,14 @@
+﻿using ProjectWithASPNET8.Model;
+using System.Collections.Generic;
+
+namespace ProjectWithASPNET8.Services
+{
+    public interface IPersonService
+    {
+        Person Create(Person person);
+        Person FindById(long id);
+        List<Person> FindAll();
+        Person Update(Person person);
+        void Delete(long id);
+    }
+}
