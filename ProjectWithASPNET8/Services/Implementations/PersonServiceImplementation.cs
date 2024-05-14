4609cbfa5e63ec0671d4fa472eea3cc68e5d6732
Vinicius <vinicius.costa@orram.com.br>
2024-05-14 10:10:24 -0300

    feat: Remove Mocks PersonServiceImp

--- ProjectWithASPNET8/Services/Implementations/PersonServiceImplementation.cs
+++ ProjectWithASPNET8/Services/Implementations/PersonServiceImplementation.cs
@@ -1,5 +1,6 @@
 ﻿using ProjectWithASPNET8.Model;
 using ProjectWithASPNET8.Model.Context;
+using System;
 
 namespace ProjectWithASPNET8.Services.Implementations
 {
@@ -12,16 +13,6 @@ namespace ProjectWithASPNET8.Services.Implementations
             _context = context;
         }
 
-        public Person Create(Person person)
-        {
-            return person;
-        }
-
-        public void Delete(long id)
-        {
-            
-        }
-
         public List<Person> FindAll()
         {
             return _context.Persons.ToList();
@@ -29,19 +20,68 @@ namespace ProjectWithASPNET8.Services.Implementations
 
         public Person FindById(long id)
         {
-            return new Person
+            return _context.Persons.SingleOrDefault(p => p.Id.Equals(id));
+        }
+
+        public Person Create(Person person)
+        {
+            try
             {
-                Id = 1,
-                FirstName = "Vinícius",
-                LastName = "Costa",
-                Address = "São Paulo - Brasil",
-                Gender = "Male"
-            };
+                _context.Add(person);
+                _context.SaveChanges();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            return person;
         }
 
         public Person Update(Person person)
         {
+            if(!Exists(person.Id)) return new Person();
+
+            var result = _context.Persons.SingleOrDefault(p => p.Id.Equals(person.Id));
+
+            if(result != null)
+            {
+                try
+                {
+                    _context.Entry(result).CurrentValues.SetValues(person);
+                    _context.SaveChanges();
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+            }
             return person;
-        }     
+        }
+
+        public void Delete(long id)
+        {
+            var result = _context.Persons.SingleOrDefault(p => p.Id.Equals(id));
+
+            if (result != null)
+            {
+                try
+                {
+                    _context.Persons.Remove(result);
+                    _context.SaveChanges();
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+            }
+        }
+
+        private bool Exists(long id)
+        {
+            return _context.Persons.Any((p => p.Id.Equals(id)));
+        }
     }
 }
